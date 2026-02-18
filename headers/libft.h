772adfbb2a6a1a22dd94f91f1a24083435d9e247
cafabre <camille.fabre003@gmail.com>
2026-02-18 17:29:50 +0100

    added ft_strcnt

--- headers/libft.h
+++ headers/libft.h
@@ -46,8 +46,10 @@ int		ft_tolower(int c);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t n);
+size_t	ft_strcnt(char const *s, char c);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_tablen(char **tab);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strnstr(const char *s1, const char *s2, size_t len);
@@ -56,19 +58,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	**ft_split(char const *s, char c);
-int		ft_tablen(char **tab);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 /* ************************************************************************** */
 /* 									MEMORY                                    */
 /* ************************************************************************** */
+int		ft_memcmp(const void *ptr1, const void *ptr2, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *ptr, int c, size_t n);
-int		ft_memcmp(const void *ptr1, const void *ptr2, size_t n);
 void	*ft_calloc(size_t n, size_t s);
 void	ft_nullfree(void **ptr);
 
@@ -93,13 +94,14 @@ void	ft_putnbr_fd(int n, int fd);
 /* 									 LISTS                                    */
 /* ************************************************************************** */
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 
 #endif
---
+++ string/ft_strcnt.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+size_t	ft_strcnt(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	if (!s)
+		return (0);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
